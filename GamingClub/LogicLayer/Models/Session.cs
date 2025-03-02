a213474b850f9f108f236a2a7c4b28fff32dbc01
Bozachydi,Liza Y. <l.bozachydi@student.fontys.nl>
2025-03-02 20:31:13 +0100

    Set Folders/Docs

---
+++ GamingClub/LogicLayer/Models/Session.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public int BookingId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+
+}
---
+++ GamingClub/LogicLayer/Models/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        public int PhoneNumber { get; set; }
+        public User User { get; set; }
+        public Role Role { get; set; }
+    }
+
+}
